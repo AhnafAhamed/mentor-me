f7af82fc3f3d09184f9f9a1481522dd114f50541
Ahnaf Ahamed <ahnafahamed4@gmail.com>
2023-07-29 19:34:55 +0530

    refactor: UpcomingMeetingCard

--- src/components/global/UpcomingMeetingCard.jsx
+++ src/components/global/UpcomingMeetingCard.jsx
@@ -1,34 +1,57 @@
-import { Flex, Stack, Text, createStyles } from '@mantine/core'
+import { Button, Flex, Stack, Text, createStyles } from '@mantine/core'
 import UserInfoCard from './UserInfoCard'
 import IconTime from '../icons/IconTime'
 import IconVideo from '../icons/IconVideo'
+import PrimaryButton from './PrimaryButton'
+import SecondaryButton from './SecondaryButton'
+import { Link } from 'react-router-dom'
 
 const useStyles = createStyles((theme) => ({
   card: {
-    borderRadius: '12px'
+    borderRadius: '12px',
+    backgroundColor: theme.colors.lightPurple
   }
 }))
 
-const UpcomingMeetingCard = ({ image, firstName, lastName, title, time }) => {
+const UpcomingMeetingCard = ({
+  image,
+  firstName,
+  lastName,
+  title,
+  time,
+  showButtons,
+  confirmClick,
+  deleteClick,
+  meetingLink
+}) => {
   const { classes } = useStyles()
   return (
-    <Stack>
-      <Stack p={16} spacing={12}>
-        <UserInfoCard
-          image={image}
-          firstName={firstName}
-          lastName={lastName}
-          title={title}
-        />
-        <Flex gap={20}>
-          <IconTime />
-          <Text>{time}</Text>
+    <Stack className={classes.card} p={16} spacing={12} maw={300}>
+      <UserInfoCard
+        image={image}
+        firstName={firstName}
+        lastName={lastName}
+        title={title}
+      />
+      <Flex gap={20}>
+        <IconTime />
+        <Text>{time}</Text>
+      </Flex>
+      <Flex gap={20}>
+        <IconVideo />
+        <Text>Online Meeting</Text>
+      </Flex>
+      {showButtons && (
+        <Flex gap={10}>
+          <PrimaryButton text="Confirm" onClick={confirmClick} />
+          <SecondaryButton text="Delete" onClick={deleteClick} />
         </Flex>
-        <Flex gap={20}>
-          <IconVideo />
-          <Text>Online Meeting</Text>
-        </Flex>
-      </Stack>
+      )}
+      {meetingLink && (
+        <Link to="https://meet.google.com/qqq-anwm-bdt" target="_blank">
+          <PrimaryButton text="Join Meeting" />
+        </Link>
+      )}
     </Stack>
   )
 }
