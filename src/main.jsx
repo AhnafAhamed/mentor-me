72a466474a5026212d68044ea0250e5e0e2755fd
Ahnaf Ahamed <ahnafahamed4@gmail.com>
2023-08-05 07:52:58 +0530

    add mantine notifications

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import './index.css'
 import { MantineProvider } from '@mantine/core'
 import THEME from './styles/theme/theme.js'
 import { BrowserRouter } from 'react-router-dom'
+import { Notifications } from '@mantine/notifications'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <MantineProvider withGlobalStyles withNormalizeCSS theme={THEME}>
       <BrowserRouter>
+        <Notifications />
         <App />
       </BrowserRouter>
     </MantineProvider>
