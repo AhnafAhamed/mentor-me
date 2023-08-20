5c66aded6b2b9922490d1db476bb4f53ede68345
Ahnaf Ahamed <ahnafahamed4@gmail.com>
2023-08-20 11:38:18 +0530

    refactor mentors page in mentee dashboard

--- src/pages/mentors/Mentors.jsx
+++ src/pages/mentors/Mentors.jsx
@@ -1,25 +1,13 @@
-import { useEffect, useState } from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout'
-import supabase from '../../config/SupabaseClient'
 import { Flex, TextInput } from '@mantine/core'
 import ProfileCard from '../../components/global/ProfileCard'
 import IconSearch from '../../components/icons/IconSearch'
+import useSupabase from '../../hooks/useSupabase'
+import { getMentors } from '../../services/Mentor'
+import CustomLoader from '../../components/global/CustomLoader'
 
 const Mentors = () => {
-  const [mentors, setMentors] = useState([])
-  const fetchMentors = async () => {
-    const { data, error } = await supabase.from('Mentor').select()
-    if (error) {
-      console.log(error)
-    } else {
-      console.log(data)
-      setMentors(data)
-    }
-  }
-
-  useEffect(() => {
-    fetchMentors()
-  }, [])
+  const { data: mentors } = useSupabase(getMentors)
   return (
     <DashboardLayout title="Mentors">
       <TextInput
@@ -29,17 +17,21 @@ const Mentors = () => {
         mb={48}
       />
       <Flex gap={35} justify="center">
-        {mentors.map((mentor) => (
-          <ProfileCard
-            key={mentor.id}
-            id={mentor.user_uid}
-            name={mentor.first_name + ' ' + mentor.last_name}
-            experience={mentor.experience}
-            jobTitle={mentor.job_title}
-            rating={Math.floor(Math.random() * 5) + 1}
-            company={mentor.workplace}
-          />
-        ))}
+        {mentors ? (
+          mentors?.map((mentor) => (
+            <ProfileCard
+              key={mentor.id}
+              id={mentor.user_uid}
+              name={mentor.first_name + ' ' + mentor.last_name}
+              experience={mentor.experience}
+              jobTitle={mentor.job_title}
+              rating={Math.floor(Math.random() * 5) + 1}
+              company={mentor.workplace}
+            />
+          ))
+        ) : (
+          <CustomLoader />
+        )}
       </Flex>
     </DashboardLayout>
   )
