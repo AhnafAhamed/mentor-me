a09bc3c7b94548c73a5cba06e28847012c0a5dae
Ahnaf Ahamed <ahnafahamed4@gmail.com>
2023-08-27 14:29:52 +0530

    fix icon link

--- src/components/icons/IconLink.jsx
+++ src/components/icons/IconLink.jsx
@@ -10,23 +10,23 @@ const IconLink = () => {
       <path
         d="M9.16663 7.33288L14.6333 1.86621"
         stroke="#4A148C"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M15.1667 4.53301V1.33301H11.9667"
         stroke="#4A148C"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M7.83337 1.33301H6.50004C3.16671 1.33301 1.83337 2.66634 1.83337 5.99967V9.99967C1.83337 13.333 3.16671 14.6663 6.50004 14.6663H10.5C13.8334 14.6663 15.1667 13.333 15.1667 9.99967V8.66634"
         stroke="#4A148C"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   )
