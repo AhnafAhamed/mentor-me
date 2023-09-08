6a81a5b203465043a552c5bc8f7ac4b1e2b7ba24
Ahnaf Ahamed <ahnafahamed4@gmail.com>
2023-09-08 10:31:42 +0530

    add edit icon

---
+++ src/components/icons/IconEdit.jsx
@@ -0,0 +1,37 @@
+const IconEdit = () => {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.5 1H4.5C2 1 1 2 1 4.5V7.5C1 10 2 11 4.5 11H7.5C10 11 11 10 11 7.5V6.5"
+        stroke="#292D32"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.01994 1.51061L4.07994 5.45061C3.92994 5.60061 3.77994 5.89561 3.74994 6.11061L3.53494 7.61561C3.45494 8.16061 3.83994 8.54061 4.38494 8.46561L5.88994 8.25061C6.09994 8.22061 6.39494 8.07061 6.54994 7.92061L10.4899 3.98061C11.1699 3.30061 11.4899 2.51061 10.4899 1.51061C9.48994 0.510615 8.69994 0.830615 8.01994 1.51061Z"
+        stroke="#292D32"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.45496 2.0752C7.78996 3.2702 8.72496 4.2052 9.92496 4.5452"
+        stroke="#292D32"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default IconEdit
